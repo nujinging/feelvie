408d6e36ae10b5415077a98b66054781918e252b
iikasam <iikasam@naver.com>
2022-11-13 15:15:23 +0900

    무한스크롤 정리

--- feelvie/src/components/Genre.vue
+++ feelvie/src/components/Genre.vue
@@ -1,11 +1,11 @@
 <template>
-  <ul v-if="genres" style="display:flex">
-    <li v-for="genreList in genres" :key="genreList.id" @click="goDetail(genreList.id)" style="margin:0 10px">
+  <ul v-if="genreTitle" style="display:flex">
+    <li v-for="genreList in genreTitle" :key="genreList.id" @click="GenreList(genreList.id)" style="margin:0 10px">
       {{ genreList.name }}
     </li>
   </ul>
-  <ul v-if="test" @scroll="handleNotificationListScroll" style="height: 500px;overflow: auto;">
-    <li v-for="item in test" :key="item.id" @click="goDetail2(item.id)">
+  <ul v-if="list" @scroll="handleNotificationListScroll" style="height: 500px;overflow: auto;">
+    <li v-for="item in list" :key="item.id" @click="goDetail(item.id)">
       <img :src="image(item.poster_path)" alt="">
     </li>
   </ul>
@@ -18,47 +18,53 @@ export default {
   name: 'Ganre_',
   data() {
     return {
-      test: {},
-      page: '',
-      page2: 1,
-      genres: {},
+      list: {},
+      page: 1,
+      genreTitle: {},
       genre: ''
     };
   },
+   // Genre Title
+   async mounted() {
+    const { data } = await movieApi.genre();
+    this.genreTitle = data.genres;
+    console.log(this.genreTitle);
+  },
+  
   methods: {
+    // 데이터가 두번 도는거 난중에 해결
+    // Ganre List - Click
+    async GenreList(value) {
+      this.page = 1;
+      this.list = [];
+      this.genre = value;
+      const { data } = await movieApi.genreList(this.genre, this.page);
+      this.list = data.results;
+      console.log(this.page)
+    },
+
+    // Scroll
     handleNotificationListScroll(e) {
       const { scrollHeight, scrollTop, clientHeight } = e.target;
       const isAtTheBottom = scrollHeight === scrollTop + clientHeight;
       if (isAtTheBottom) this.handleLoadMore();
     },
-    // 스크롤이 닿으면 호출되는 함수
     async handleLoadMore() {
-      this.page2++
-      const {data} = await movieApi.genreList(this.genre, this.page2)
-      this.test = [...this.test, ...data.results];  // spread연산자 ==> 배열이나, 객체를 풀어내는 것
-  
-      console.log(this.page2)
-      console.log(data);
+      this.page++
+      const { data } = await movieApi.genreList(this.genre, this.page)
+      this.list = [...this.list, ...data.results]
+      console.log(this.page)
     },
 
-    async goDetail(value) {
-      this.genre = value
-      const { data } = await movieApi.genreList(this.genre, this.page2);
-      this.test = data.results;
-    },
-
-    goDetail2(id) {
-      this.$router.push(`/detail/${id}`);
-    },
+    // List Image
     image(img) {
       return `https://image.tmdb.org/t/p/w300/${img}`
     },
 
-  },
-  async mounted() {
-    const { data } = await movieApi.genre();
-    this.genres = data.genres;
-    console.log(this.genres);
+    // Detail Page
+    goDetail(id) {
+      this.$router.push(`/detail/${id}`);
+    },
   },
 }
 </script>
