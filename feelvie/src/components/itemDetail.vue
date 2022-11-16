db526ce643fae45a0197d36594c83e5a9d95d43f
iikasam <iikasam@naver.com>
2022-11-16 23:55:48 +0900

    배경화면 완료

--- feelvie/src/components/itemDetail.vue
+++ feelvie/src/components/itemDetail.vue
@@ -1,6 +1,6 @@
 <template>
   <Header></Header>
-  <div class="container" :style="background">
+  <div class="container" :style="{ backgroundImage: 'url( https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/' + this.backGround + ')' }">
     <section class="detail_container">
       <div class="detail_info">
         <h1>{{ movieDetail.title }}</h1>
@@ -79,6 +79,7 @@ export default {
       Bg: 'red',
       similarList: {},
       personList: {},
+      backGround:'',
     };
   },
   methods: {
@@ -87,7 +88,8 @@ export default {
     },
     profile(img) {
       return `https://image.tmdb.org/t/p/w138_and_h175_face/${img}`
-    }
+    },
+    
   },
   async mounted() {
     const { id } = this.$route.params;
@@ -95,7 +97,8 @@ export default {
     this.movieDetail = data;
     console.log(this.movieDetail)
     this.backGround = this.movieDetail.backdrop_path
-
+    console.log(this.backGround)
+    
     const similar = await movieApi.similar(this.movieDetail.id);
     this.similarList = similar.data.results
 
@@ -112,18 +115,18 @@ export default {
 }
 </script>
 <style>
-:root {--test-color: red;}
-.detail_container {display:flex;margin-bottom:110px;padding:3.125vw 3.125vw 0;color:#fff;background:var(--test-color)}
-.detail_container .detail_info {position:relative;flex:2;margin-right:50px;width:100%}
+.detail_container {position:relative;display:flex;margin-bottom:110px;padding:3.125vw 3.125vw 10.125vw ;color:#fff}
+
+.detail_container .detail_info {position:relative;flex:2;margin-right:50px;width:100%;z-index:1}
 .detail_container .detail_info h1 {margin-top:50px;font-size:40px}
 .detail_container .detail_info .meta {margin-top:15px}
 .detail_container .detail_info .meta .badge {display:inline-block;margin-right:8px;padding:0 8px;color:#fff;font-size:14px;line-height:20px;border:1px solid #fff;border-radius:4px;background:#000}
 .detail_container .detail_info .trailer {margin-top:30px;display:inline-block;padding:20px 0;width:315px;font-size:24px;border-radius:4px;text-align:center;background:#fff}
 .detail_container .detail_info .comment {position:absolute;bottom:0}
 .detail_container .detail_info .comment .quotes {margin-bottom:32px;color:#3A7CDF;font-size:24px;font-style:italic;}
-.detail_container .detail_info .comment .intro {font-size:20px;line-height:24px}
+.detail_container .detail_info .comment .intro {display:-webkit-box;font-size:20px;line-height:24px;overflow:hidden;text-overflow:ellipsis;-webkit-line-clamp:3;-webkit-box-orient:vertical}
 
-.detail_container .detail_poster {position:relative;display:flex;justify-content:flex-end;flex:1;width:100%}
+.detail_container .detail_poster {position:relative;display:flex;justify-content:flex-end;flex:1;width:100%;z-index:1}
 .detail_container .detail_poster picture {margin-left:30px}
 .detail_container .detail_poster .social_links {}
 .detail_container .detail_poster .social_links li {display:flex;align-items:center;justify-content:center;width:72px;height:72px;border-radius:50%;background:#fff}
@@ -134,7 +137,15 @@ export default {
 .detail_container .detail_poster .social_links li a.instargram {}
 .detail_container .detail_poster .social_links li a.link {}
 
-.detail_container:before {position:absolute;top:0;left:0;width:100%;height:100%;background-image:linear-gradient(180deg, rgb(0 0 0 / 54%) 142px, rgb(0 0 0) 100%);content:''}
+/* .detail_container:before {position:absolute;top:0;left:0;width:100%;height:100%;background-image:linear-gradient(180deg, rgb(0 0 0 / 54%) 142px, rgb(0 0 0) 100%);content:''}
+ */
+
+.detail_container::before {
+    content: "";
+    position: absolute;
+    inset: -10rem;
+    background-color: rgba(0, 0, 0, 0.5);}
+.detail_container::after { content: ""; position: absolute; bottom: -2px; left: -1rem; right: -1rem; height: 25rem; background-image: linear-gradient(to top, rgb(0, 0, 0), rgba(0, 0, 0, 0)); }
 
 
 @media (max-width: 1024px) {
