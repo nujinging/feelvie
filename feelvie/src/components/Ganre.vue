c733398b9ce82291eebbe2e5c36de30bdb3d0baa
iikasam <iikasam@naver.com>
2022-12-20 11:03:08 +0900

    tv, movie 분리

--- feelvie/src/components/Ganre.vue
+++ feelvie/src/components/Ganre.vue
@@ -16,7 +16,15 @@
         </div>
         <!-- 오늘 -->
         <div class="item_container">
-            <ItemList :movieList="dayList" :type2="movie" :title="title[0]" :photo="day_photo"></ItemList>
+            <ul class="type_list">
+                <li>
+                  <button type="button" class="active"  @click="ganreTab('movie')">영화</button>
+                </li>
+                <li>
+                  <button type="button"  @click="ganreTab('tv')">TV</button>
+                </li>
+              </ul>
+            <ItemList v-if="dayList" :movieList="dayList" :type="list_type" :title="title[0]" :photo="day_photo"></ItemList>
         </div>
         <!-- 이번주 -->
         <div class="item_container">
@@ -43,27 +51,31 @@ export default {
             newList: {},
             dayList: [],
             weekList: {},
+            list_type:'movie',
             title: ['오늘 ! 가장 있기있는 컨텐츠', '이번주 가장 있기있는 컨텐츠']
         };
     },
     
     // Genre Title
-    async created() {
+    async mounted() {
         const { data } = await movieApi.genre(this.linkValue);
         this.genreTitle = data.genres;
 
-        const trendingDay = await movieApi.trending('movie', 'day');
-        const trendingWeek = await movieApi.trending('movie', 'week');
-
-        this.dayList = trendingDay.data.results
-        this.weekList = trendingWeek.data.results
-
-        this.day_photo = this.dayList.map(key => key.poster_path)
-        this.week_photo = this.weekList.map(key => key.poster_path)
     },
 
     methods: {
+        async ganreTab(id) {
+            this.list_type = id;
+            
+            const trendingDay = await movieApi.trending(this.list_type, 'day');
+            const trendingWeek = await movieApi.trending(this.list_type, 'week');
+            
+            this.dayList = trendingDay.data.results
+            this.weekList = trendingWeek.data.results
 
+            this.day_photo = this.dayList.map(key => key.poster_path)
+            this.week_photo = this.weekList.map(key => key.poster_path)
+        },
 
         // 데이터가 두번 도는거 난중에 해결
         // Ganre List - Click
