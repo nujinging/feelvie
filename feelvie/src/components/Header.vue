f1e008b294d7165113b5613eb4c22350553501b5
iikasam <iikasam@naver.com>
2022-12-23 15:39:24 +0900

    반응형

--- feelvie/src/components/Header.vue
+++ feelvie/src/components/Header.vue
@@ -32,18 +32,21 @@ export default {
 
 <style>
 .wrap_header {position:sticky;top:0;width:100%;z-index:9999}
-.wrap_header .header_container {padding:20px 32px;display:flex;align-items:center;justify-content:space-between;}
-.wrap_header .navigation {display:flex;align-items:center;margin-left:35px}
+.wrap_header .header_container {padding:1.25rem 5rem;display:flex;align-items:center;justify-content:space-between;}
+.wrap_header .navigation {display:flex;align-items:center}
 .wrap_header .menu {display:flex;align-items:center;margin-left:35px}
-.wrap_header .menu li a {color:#fff;font-size:28px;font-weight:bold;line-height:20px}
+.wrap_header .menu li a {color:#fff;font-size:1.75rem;font-weight:bold;line-height:1.25rem}
 .wrap_header .search {display:inline-block;width:50px;height:50px;background:url("data:image/svg+xml,%3Csvg width='32' height='32' viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'%3E%3Crect width='32' height='32' fill='url(%23pattern0)'/%3E%3Cdefs%3E%3Cpattern id='pattern0' patternContentUnits='objectBoundingBox' width='1' height='1'%3E%3Cuse xlink:href='%23image0_12_305' transform='scale(0.03125)'/%3E%3C/pattern%3E%3Cimage id='image0_12_305' width='32' height='32' xlink:href='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAA7EAAAOxAGVKw4bAAAB2klEQVRYhe2WO0vEQBSF7wRLSwmyyGItVmJtoY1YiMgW1oIoYiEisv9EtrCwsLMVK3+F4GIhYieoWIjI4uOzmBHD3Ukyk6zaeCHFTvac881kXkYKCkhFpCUicyIyISKpe3UvIhciciYix8aY2yKf6AJSoAP0KK8ecAA0BhW+ANwFBOt6BJbqhq8C7xXCv+od2KjTc1/4FbAHTAMj7pkCdoHLHIjl2PCU/mF/BdrAUIFuCNihf648EjMnsBNOhy9G6Oc9EIeh4tQjbgfTf/tsezoxFiLcVMKromEv8EmArvLaKdMlYjeZbHWMMW+xAMaYDxHZV83au7881NOx4RmvSeV1HSJ6UKKRGgDDyuu5TJNUDcupj1hBIvZgyVazBsC4+q29vQAXqm22BoDWam8vwJlqW6u6DEVkXTVrb69w1LMRla5fj4/eT16BsM+JPc+z1QPmI8JngRflcRRD38AeIBpi2w1tni5xPdfhT8G9z5gt4T+Ouw5kErvOh4EJYAs49/z/a/hXogAcxEYORJWqDLFM/+cIqScXOhCIBnDoMczr6RHQBFYGBuFAxrA3nhPgGnh2zw1wir2WNZVmsBAVwf8h/iFCIVp/DVF+ZfthiO6vAjiIFnZP6QIzn9TA/Qx6PEfbAAAAAElFTkSuQmCC'/%3E%3C/defs%3E%3C/svg%3E%0A") center no-repeat}
 .wrap_header .menu li+li {margin-left:20px}
 @media (max-width: 767px) {
-  .wrap_header {margin:0 15px;padding:10px 0}
-  .wrap_header .logo {width:70px}
+  .wrap_header .logo {width:3.75rem}
   .wrap_header .logo img {width:100%}
   .wrap_header .menu {margin-left:20px}
-  .wrap_header .menu li a {font-size:18px}
+  .wrap_header .menu li a {font-size:16px}
+  .wrap_header .menu li+li {margin-left:10px}
   .wrap_header .menu li+li a {margin-left:8px}
+  .wrap_header .header_container {padding:5px 16px 5px 30px}
+
+  .wrap_header .search {width:30px;height:30px;background-size:16px}
 }
 </style>
--- feelvie/src/components/ItemList.vue
+++ feelvie/src/components/ItemList.vue
@@ -55,5 +55,11 @@ export default {
 .item_card:hover {transform: translate3d(0,-0.75rem,0);transition: transform .3s ease-in-out;cursor:pointer}
 .item_card img {display:block;width:100%;height:450px;border-radius:8px}
 .item_card h3 {margin-top:15px;color: #d5d5d5;font-size:22px;font-weight:normal;overflow:hidden;text-overflow:ellipsis;white-space:nowrap}
-@media (max-width: 767px) {.swiper-slide {width: 120px}.item_card img {width: 120px;height: 180px}}
+
+@media (max-width: 767px) {
+  .item_container {padding:1.25rem}
+  .swiper+.title {margin-top:1.325rem}
+  .swiper-slide {width: 120px}.item_card img {width: 120px;height: 180px}
+  .item_card h3 {font-size:16px}
+}
 </style>
