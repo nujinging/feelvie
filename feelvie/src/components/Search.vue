cddf2ad153c2eee006187d6277af37f219a3bb97
iikasam <iikasam@naver.com>
2022-12-23 10:10:02 +0900

    search - detail

--- feelvie/src/components/Search.vue
+++ feelvie/src/components/Search.vue
@@ -14,14 +14,16 @@
         검색결과가 없습니다.
       </div>
     </section>
-
-    <ItemList :movieList="movieList" :photo="search_photo"></ItemList>
+    <ul class="search_list" v-if="movieList">
+      <li v-for="item in movieList" :key="item.id" @click="goDetail(item.media_type, item.id)">
+          <img :src="image(item.poster_path)" alt="">
+      </li>
+  </ul>
   </div>
   
 </template>
 
 <script>
-import ItemList from './ItemList.vue'
 import { movieApi } from '../utils/axios';
 import { debounce } from 'lodash'
 
@@ -32,9 +34,6 @@ export default {
       movieList: '',
     };
   },
-  components: {
-    ItemList
-  },
   methods: {
     // 값을 입력하고 0.5초 뒤 api 호출
     autoSearch: debounce(async function (e) {
@@ -43,6 +42,15 @@ export default {
       this.search_photo = this.movieList.map(key => key.poster_path)
       console.log(this.movieList)
     },100),
+         // List Image
+         image(img) {
+            return `https://image.tmdb.org/t/p/w300/${img}`
+        },
+
+        // Detail Page
+        goDetail(type, id) {
+            this.$router.push(`/${type}/${id}`);
+        },
   }
 }
 </script>
@@ -54,6 +62,7 @@ export default {
 .search_container .search_input .icon_search {position:absolute;right:0;bottom:0;width:50px;height:50px;background:url("data:image/svg+xml,%3Csvg width='32' height='32' viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'%3E%3Crect width='32' height='32' fill='url(%23pattern0)'/%3E%3Cdefs%3E%3Cpattern id='pattern0' patternContentUnits='objectBoundingBox' width='1' height='1'%3E%3Cuse xlink:href='%23image0_12_305' transform='scale(0.03125)'/%3E%3C/pattern%3E%3Cimage id='image0_12_305' width='32' height='32' xlink:href='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAA7EAAAOxAGVKw4bAAAB2klEQVRYhe2WO0vEQBSF7wRLSwmyyGItVmJtoY1YiMgW1oIoYiEisv9EtrCwsLMVK3+F4GIhYieoWIjI4uOzmBHD3Ukyk6zaeCHFTvac881kXkYKCkhFpCUicyIyISKpe3UvIhciciYix8aY2yKf6AJSoAP0KK8ecAA0BhW+ANwFBOt6BJbqhq8C7xXCv+od2KjTc1/4FbAHTAMj7pkCdoHLHIjl2PCU/mF/BdrAUIFuCNihf648EjMnsBNOhy9G6Oc9EIeh4tQjbgfTf/tsezoxFiLcVMKromEv8EmArvLaKdMlYjeZbHWMMW+xAMaYDxHZV83au7881NOx4RmvSeV1HSJ6UKKRGgDDyuu5TJNUDcupj1hBIvZgyVazBsC4+q29vQAXqm22BoDWam8vwJlqW6u6DEVkXTVrb69w1LMRla5fj4/eT16BsM+JPc+z1QPmI8JngRflcRRD38AeIBpi2w1tni5xPdfhT8G9z5gt4T+Ouw5kErvOh4EJYAs49/z/a/hXogAcxEYORJWqDLFM/+cIqScXOhCIBnDoMczr6RHQBFYGBuFAxrA3nhPgGnh2zw1wir2WNZVmsBAVwf8h/iFCIVp/DVF+ZfthiO6vAjiIFnZP6QIzn9TA/Qx6PEfbAAAAAElFTkSuQmCC'/%3E%3C/defs%3E%3C/svg%3E%0A") center no-repeat}
 /*.item_container {padding-left:30px}*/
 .search_none {height:50vh;display:flex;align-items:center;justify-content:center;color:#757575;font-size:28px;font-weight:bold}
+.search_list li {display:inline-block}
 @media (max-width: 1024px) {
   
 }
