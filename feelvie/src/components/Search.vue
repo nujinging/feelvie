4197316ff566a226c59d89540037019773b88112
iikasam <iikasam@naver.com>
2022-12-26 22:24:37 +0900

    반응형작업중

--- feelvie/src/components/Search.vue
+++ feelvie/src/components/Search.vue
@@ -1,34 +1,30 @@
 <template>
-  <div class="container">
     <section class="search_container">
-      <form>
-        <label class="search_input" for="search_input">
-          <input id="search_input" type="text" @keyup="autoSearch($event)"
-            onkeypress="if(event.keyCode=='13'){event.preventDefault()}" placeholder="TV프로그램, 영화 제목 및 출연진으로 검색해보세요"
-            class="search_txt">
-          <button type="button" class="icon_search">
-            <span class="blind">검색</span>
-          </button>
-        </label>
-      </form>
+            <form>
+                <label class="search_input" for="search_input">
+                    <input id="search_input" type="text" @keyup="autoSearch($event)"
+                        onkeypress="if(event.keyCode=='13'){event.preventDefault()}"
+                        placeholder="TV프로그램, 영화 제목 및 출연진으로 검색해보세요" class="search_txt">
+                    <button type="button" class="icon_search">
+                        <span class="blind">검색</span>
+                    </button>
+                </label>
+            </form>
 
-      <div class="search_none" v-if="searchNone">
-        검색결과가 없습니다.
-      </div>
-    </section>
+            <div class="search_none" v-if="searchNone">
+                검색결과가 없습니다.
+            </div>
 
-    <div class="item_container">
-      <ul class="search_list" v-if="movieList">
-        <li v-for="item in movieList" :key="item.id" @click="goDetail(item.media_type, item.id)" class="list_card">
-          <img :src="image(item.poster_path)" alt="">
-          <p class="tit"> {{ item.title }}</p>
-          <p class="tit" v-if="this.movieListName == searchResult">{{ searchResult  }}</p>
-        </li>
-      </ul>
-    </div>
 
-  </div>
-
+            <ul class="search_list" v-if="movieList">
+                <li v-for="item in movieList" :key="item.id" @click="goDetail(item.media_type, item.id)"
+                    class="list_card">
+                    <img :src="image(item.poster_path)" alt="">
+                    <p class="tit"> {{ item.title }}</p>
+                    <p class="tit" v-if="this.movieListName == searchResult">{{ searchResult }}</p>
+                </li>
+            </ul>
+        </section>
 </template>
 
 <script>
@@ -36,65 +32,66 @@ import { movieApi } from '../utils/axios';
 import { debounce } from 'lodash'
 
 export default {
-  name: 'Search_',
-  data() {
-    return {
-      movieList: '',
-      images: ['image_none.png'],
-      searchResult: '',
-      searchNone: false,
-      test: ''
-    };
-  },
-  methods: {
-    // 값을 입력하고 0.1초 뒤 api 호출
-    autoSearch: debounce(async function (e) {
-      const searchText = e.target.value;
-      // 공백이 없거나 달라도 검색되게 공백제거
-      const searchResult = searchText.replace(/(\s*)/g, "");
-      // 검색결과
-      const { data } = await movieApi.search(searchResult);
-      this.movieList = data.results;
-      this.search_photo = this.movieList.map(key => key.poster_path);
-      // 검색결과가 없을 때
-      this.searchNone = this.movieList.length === 0 ? true : false;
-
-      this.movieListName = this.movieList.map(key => key.title)
-    }, 100),
-
-    image(img) {
-      if (img == undefined) {
-        // 이미지 없을 때
-        return require(`@/assets/${this.images}`)
-      } else {
-        return `https://image.tmdb.org/t/p/w300/${img}`
-      }
+    name: 'Search_',
+    data() {
+        return {
+            movieList: '',
+            images: ['image_none.png'],
+            searchResult: '',
+            searchNone: false,
+            test: ''
+        };
     },
+    methods: {
+        // 값을 입력하고 0.1초 뒤 api 호출
+        autoSearch: debounce(async function (e) {
+            const searchText = e.target.value;
+            // 공백이 없거나 달라도 검색되게 공백제거
+            const searchResult = searchText.replace(/(\s*)/g, "");
+            // 검색결과
+            const { data } = await movieApi.search(searchResult);
+            this.movieList = data.results;
+            this.search_photo = this.movieList.map(key => key.poster_path);
+            // 검색결과가 없을 때
+            this.searchNone = this.movieList.length === 0 ? true : false;
 
-    // Detail Page
-    goDetail(type, id) {
-      this.$router.push(`/${type}/${id}`);
-    },
-  }
+            this.movieListName = this.movieList.map(key => key.title)
+        }, 100),
+
+        image(img) {
+            if (img == undefined) {
+                // 이미지 없을 때
+                return require(`@/assets/${this.images}`)
+            } else {
+                return `https://image.tmdb.org/t/p/w300/${img}`
+            }
+        },
+
+        // Detail Page
+        goDetail(type, id) {
+            this.$router.push(`/${type}/${id}`);
+        },
+    }
 }
 </script>
 
 <style>
-.search_container {margin:0 auto;padding:3.125vw;width:80%}
-.search_container .search_input {position:relative;display:block;padding:3.75em 0 1em;border-bottom:2px solid #fff}
-.search_container .search_input .search_txt {width:calc(100% - 60px);color:#eee;font-size:28px;font-weight:bold;border:none;background:none;font-family: 'Noto Sans KR', sans-serif}
-.search_container .search_input .icon_search {position:absolute;right:0;bottom:0;width:50px;height:50px;background:url("data:image/svg+xml,%3Csvg width='32' height='32' viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'%3E%3Crect width='32' height='32' fill='url(%23pattern0)'/%3E%3Cdefs%3E%3Cpattern id='pattern0' patternContentUnits='objectBoundingBox' width='1' height='1'%3E%3Cuse xlink:href='%23image0_12_305' transform='scale(0.03125)'/%3E%3C/pattern%3E%3Cimage id='image0_12_305' width='32' height='32' xlink:href='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAA7EAAAOxAGVKw4bAAAB2klEQVRYhe2WO0vEQBSF7wRLSwmyyGItVmJtoY1YiMgW1oIoYiEisv9EtrCwsLMVK3+F4GIhYieoWIjI4uOzmBHD3Ukyk6zaeCHFTvac881kXkYKCkhFpCUicyIyISKpe3UvIhciciYix8aY2yKf6AJSoAP0KK8ecAA0BhW+ANwFBOt6BJbqhq8C7xXCv+od2KjTc1/4FbAHTAMj7pkCdoHLHIjl2PCU/mF/BdrAUIFuCNihf648EjMnsBNOhy9G6Oc9EIeh4tQjbgfTf/tsezoxFiLcVMKromEv8EmArvLaKdMlYjeZbHWMMW+xAMaYDxHZV83au7881NOx4RmvSeV1HSJ6UKKRGgDDyuu5TJNUDcupj1hBIvZgyVazBsC4+q29vQAXqm22BoDWam8vwJlqW6u6DEVkXTVrb69w1LMRla5fj4/eT16BsM+JPc+z1QPmI8JngRflcRRD38AeIBpi2w1tni5xPdfhT8G9z5gt4T+Ouw5kErvOh4EJYAs49/z/a/hXogAcxEYORJWqDLFM/+cIqScXOhCIBnDoMczr6RHQBFYGBuFAxrA3nhPgGnh2zw1wir2WNZVmsBAVwf8h/iFCIVp/DVF+ZfthiO6vAjiIFnZP6QIzn9TA/Qx6PEfbAAAAAElFTkSuQmCC'/%3E%3C/defs%3E%3C/svg%3E%0A") center no-repeat}
-/*.item_container {padding-left:30px}*/
-.search_none {height:50vh;display:flex;align-items:center;justify-content:center;color:#757575;font-size:28px;font-weight:bold}
-.search_list li {display:inline-block}
-.search_list {display:flex;flex-wrap:wrap;gap:2.25rem;margin-top:3.75rem;padding:4.7rem}
+.search_container {margin:12.5rem 2.5rem 0}
+.search_container .search_input {position:relative;display:block;border-bottom:2px solid #fff}
+.search_container .search_input .search_txt {width:calc(100% - 3.75rem);color:#eee;font-size:1.75rem;font-weight:bold;border:none;background:none;font-family:'Noto Sans KR', sans-serif}
+.search_container .search_input .icon_search {position:absolute;right:0;bottom:0;width:3.125rem;height:3.125rem;background:url("data:image/svg+xml,%3Csvg width='32' height='32' viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'%3E%3Crect width='32' height='32' fill='url(%23pattern0)'/%3E%3Cdefs%3E%3Cpattern id='pattern0' patternContentUnits='objectBoundingBox' width='1' height='1'%3E%3Cuse xlink:href='%23image0_12_305' transform='scale(0.03125)'/%3E%3C/pattern%3E%3Cimage id='image0_12_305' width='32' height='32' xlink:href='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAA7EAAAOxAGVKw4bAAAB2klEQVRYhe2WO0vEQBSF7wRLSwmyyGItVmJtoY1YiMgW1oIoYiEisv9EtrCwsLMVK3+F4GIhYieoWIjI4uOzmBHD3Ukyk6zaeCHFTvac881kXkYKCkhFpCUicyIyISKpe3UvIhciciYix8aY2yKf6AJSoAP0KK8ecAA0BhW+ANwFBOt6BJbqhq8C7xXCv+od2KjTc1/4FbAHTAMj7pkCdoHLHIjl2PCU/mF/BdrAUIFuCNihf648EjMnsBNOhy9G6Oc9EIeh4tQjbgfTf/tsezoxFiLcVMKromEv8EmArvLaKdMlYjeZbHWMMW+xAMaYDxHZV83au7881NOx4RmvSeV1HSJ6UKKRGgDDyuu5TJNUDcupj1hBIvZgyVazBsC4+q29vQAXqm22BoDWam8vwJlqW6u6DEVkXTVrb69w1LMRla5fj4/eT16BsM+JPc+z1QPmI8JngRflcRRD38AeIBpi2w1tni5xPdfhT8G9z5gt4T+Ouw5kErvOh4EJYAs49/z/a/hXogAcxEYORJWqDLFM/+cIqScXOhCIBnDoMczr6RHQBFYGBuFAxrA3nhPgGnh2zw1wir2WNZVmsBAVwf8h/iFCIVp/DVF+ZfthiO6vAjiIFnZP6QIzn9TA/Qx6PEfbAAAAAElFTkSuQmCC'/%3E%3C/defs%3E%3C/svg%3E%0A") center no-repeat}
 
-.search_list .tit {margin-top:0.938rem;color: #d5d5d5;font-size:1.375rem;font-weight:normal;overflow:hidden;text-overflow:ellipsis;white-space:nowrap}
+.search_container .search_list {display:flex;flex-wrap:wrap;gap:2.25rem;margin-top:3.75rem}
+.search_container .search_list li {display:inline-block}
+.search_container .search_list .tit {margin-top:0.938rem;color: #d5d5d5;font-size:1.375rem;font-weight:normal;overflow:hidden;text-overflow:ellipsis;white-space:nowrap}
+
+.search_container .search_none {padding-top:20rem;display:flex;align-items:center;justify-content:center;color:#757575;font-size:1.75rem;font-weight:bold}
 
 .item_container {margin:0 auto}
 .list_card {width:18.75rem}
-    @media screen and (max-width: 768px) {
-        .list_card {width:16.4rem}
-        .ganre_list {padding:0}
-      }
+
+@media screen and (max-width: 768px) {
+    .list_card {width:16.4rem}
+    .ganre_list {padding:0}
+}
 </style>
--- feelvie/src/components/bgModal.vue
+++ feelvie/src/components/bgModal.vue
@@ -1,13 +1,22 @@
 <template>
     <div class="intro_modal">
         <div class="inner">
-            
+
             <a :href="`https://image.tmdb.org/t/p/original/${img}`" class="facebook" target="_blank">
                 <img :src="image(img)" @click="detailImg(this.img)">
-              </a>
-            <p>{{  width }}</p>
-            <p>{{  height }}</p>
-            <button type="button" @click="$emit('close')">닫기</button>
+            </a>
+
+            <span class="txt">이미지 클릭하여 원본보기</span>
+
+            <p class="size">
+                {{ width }}
+                <span>X</span>
+                {{ height }}
+            </p>
+            
+            <button type="button" @click="$emit('close')">
+                <span class="blind">닫기</span>
+            </button>
         </div>
     </div>
 </template>
@@ -19,10 +28,10 @@ export default {
         return {
         }
     },
-    props: ['img','width','height'],
+    props: ['img', 'width', 'height'],
     methods: {
         image(img) {
-      return `https://image.tmdb.org/t/p/w400/${img}`
+            return `https://image.tmdb.org/t/p/w400/${img}`
         },
 
         detailImg(img) {
@@ -36,5 +45,13 @@ export default {
 <style>
 .intro_modal {position:fixed;top:0;left:0;z-index:600}
 .intro_modal:before  {position:fixed;top:0;left:0;width:100%;height:100%;background:#000000a1;content:''}
-.intro_modal .inner {position:fixed;top:50%;left:50%;padding:20px;width:500px;color:#ddd;border-radius:20px;background:#fff;transform:translate(-50%, -50%);}
+.intro_modal .inner {position:fixed;top:50%;left:50%;padding:1.25rem;width:31.25rem;border-radius:1.25rem;background:#fff;transform:translate(-50%, -50%);box-sizing:border-box}
+.intro_modal .inner .size {display:flex;align-items:center;margin:2rem 0 1rem;color:#000;font-size:2.063rem}
+.intro_modal .inner .size:before {display:inline-block;margin-right:10px;font-weight:bold;content:'SIZE :'}
+.intro_modal .inner .txt {display:block;margin-top:1rem;color:#8d8d8d;font-size:16px;text-align:right}
+.intro_modal .inner button {position:absolute;bottom:-50px;left:50%;width:50px;height:50px;transform:translate(-50%, 0);background:url("data:image/svg+xml,%3Csvg width='22' height='22' viewBox='0 0 22 22' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Crect x='18.0103' width='5' height='25' transform='rotate(45 18.0103 0)' fill='white'/%3E%3Crect width='5' height='25' transform='matrix(0.707107 -0.707107 -0.707107 -0.707107 18.0103 21.3345)' fill='white'/%3E%3C/svg%3E%0A") center no-repeat}
+@media screen and (max-width: 768px) {
+    .intro_modal .inner {width:calc(100% - 80px)}
+    .intro_modal .inner img {width:100%}
+}
 </style>
