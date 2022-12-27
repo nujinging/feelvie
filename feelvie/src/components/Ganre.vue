6fa5d18f06c4a0f1a51988fdd0811104495fe347
iikasam <iikasam@naver.com>
2022-12-27 11:13:14 +0900

    장르정리

--- feelvie/src/components/Ganre.vue
+++ feelvie/src/components/Ganre.vue
@@ -1,20 +1,23 @@
 <template>
     <div class="container">
         <div class="item_container">
-            <swiper :slidesPerView="'auto'" :spaceBetween="20" class="mySwiper ganre_title" v-if="genreTitle">
-
-                
-                <swiper-slide class="category_list" @click="GenreListAll()" :class="{ active : isActive }">All</swiper-slide>
-                
-                <swiper-slide class="category_list" v-for="(genreList, i ) in genreTitle" :key="genreList.id"
-                    @click="GenreList(genreList.id, i)"  :class="{ active: i === activeItem}">
+            <swiper :slidesPerView="'auto'" class="mySwiper genre_title" v-if="genreTitle">
+                <swiper-slide class="genre_item" @click="GenreListAll()"
+                    :class="{ active: genreAllActive }">
+                    All
+                </swiper-slide>
+                <swiper-slide class="genre_item" v-for="(genreList, i ) in genreTitle" :key="genreList.id"
+                    @click="GenreList(genreList.id, i)" :class="{ active: i === genreTitleActive }">
                     {{ genreList.name }}
                 </swiper-slide>
             </swiper>
 
-            <ul class="ganre_list" v-if="list2" @scroll="handleNotificationListScroll">
-                <li v-for="item in list2" :key="item.id" @click="goDetail(this.linkValue, item.id)" class="list_card">
-                    <img :src="image(item.poster_path)" alt="">
+            <ul class="ganre_list" v-if="genreList" @scroll="handleNotificationListScroll">
+                <li v-for="item in genreList" :key="item.id" @click="goDetail(this.linkValue, item.id)"
+                    class="list_card">
+                    <picture>
+                        <img :src="image(item.poster_path)" alt="">
+                    </picture>
                     <p class="tit"> {{ this.linkValue == "movie" ? item.title : item.name }} </p>
                 </li>
             </ul>
@@ -29,62 +32,48 @@ import { movieApi } from '../utils/axios';
 
 export default {
     name: 'Ganre_',
-    props: ['ganreList', 'linkValue'],
+    props: ['linkValue'],
     data() {
         return {
-            list: {},
-            list2: {},
-            page: 1,
-            genreTitle: {},
             genre: '',
-            newList: {},
-            dayList: [],
-            weekList: {},
-            popularList: {},
-            list_type: 'movie',
-            isActive: true,
-            selected: '',
-            result:'',
-            typeList: ['영화', 'TV'],
-            title: ['지금 상영중인 영화', '인기있는 영화', '최고의 등급'],
-            activeItem: null,
+            genreTitle: {},
+            genreList: {},
+            page: 1,
+            genreAllActive: true,
+            genreTitleActive: null,
         };
     },
 
-    // Genre Title
     async mounted() {
+        // 장르타이틀
         const { data } = await movieApi.genre(this.linkValue);
         this.genreTitle = data.genres;
 
-        const popular = await movieApi.popular(this.linkValue);
-        this.list = popular.data.results;
-
+        // 장르리스트 (타이틀이 한글일 경우에만)
         const korean = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
-        this.list2 = this.list.filter(value => korean.test(value.name) || korean.test(value.title));
-        console.log(this.list2)
-        
+        const popular = await movieApi.popular(this.linkValue);
+        this.genreList = popular.data.results.filter(value => korean.test(value.name) || korean.test(value.title));
     },
 
     methods: {
-        // 데이터가 두번 도는거 난중에 해결
-        // Ganre List - Click
-        async GenreListAll () {
+        // 장르 All 클릭
+        async GenreListAll() {
             const popular = await movieApi.popular(this.linkValue);
-            this.list2 = popular.data.results;
-            this.activeItem = null;
-            this.isActive = true
+            this.genreList = popular.data.results;
+            this.genreTitleActive = null;
+            this.genreAllActive = true
         },
+        // 장르 타이틀 클릭
         async GenreList(value, i) {
             this.page = 1;
-            this.list = [];
             this.genre = value;
             const { data } = await movieApi.genreList(this.linkValue, this.genre, this.page);
-            this.list = data.results;
             const korean = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
-            this.list2 = this.list.filter(value => korean.test(value.name) || korean.test(value.title));
-            this.activeItem = i;
-            this.isActive = false;
+            this.genreList = data.results.filter(value => korean.test(value.name) || korean.test(value.title));
+            this.genreTitleActive = i;
+            this.genreAllActive = false;
         },
+
         // Scroll
         handleNotificationListScroll(e) {
             const { scrollHeight, scrollTop, clientHeight } = e.target;
@@ -97,12 +86,12 @@ export default {
             this.list = [...this.list, ...data.results]
         },
 
-        // List Image
+        // 포스터
         image(img) {
             return `https://image.tmdb.org/t/p/w300/${img}`
         },
 
-        // Detail Page
+        // 영화상세내용으로
         goDetail(type, id) {
             this.$router.push(`/${type}/${id}`);
         },
@@ -115,21 +104,20 @@ export default {
 </script>
 
 <style>
-.ganre_title {position:sticky;top:5rem;background:#000}
-.category_list {width: auto;padding: 0.625rem 1.25rem;color: #fff;font-size: 1.25rem;border-radius: 2rem;cursor: pointer;background: #0372D2}
-.category_list:hover {color: #0372D2;background: #fff}
-.category_list.active {color:#0372d2;background:#fff}
-.ganre_list {display:flex;flex-wrap:wrap;gap:2.25rem;margin-top:3.75rem;padding:4.7rem}
+.genre_title {margin:5rem 0 3rem}
+.genre_title .genre_item {width: auto;padding: 0.625rem 1.25rem;color: #fff;font-size: 1.25rem;border-radius: 2rem;cursor: pointer;background: #0372D2}
+.genre_title .genre_item+.genre_item {margin-left:1.25rem}
+.genre_title .genre_item:hover {color: #0372D2;background: #fff}
+.genre_title .genre_item.active {color:#0372d2;background:#fff}
 .ganre_list .tit {margin-top:0.938rem;color: #d5d5d5;font-size:1.375rem;font-weight:normal;overflow:hidden;text-overflow:ellipsis;white-space:nowrap}
-
-.item_container {margin:0 auto}
-.list_card {width:18.75rem}
-.list_card img {border-radius:0.5rem;width:100%}
-    .list_card:hover {transform: translate3d(0,-0.75rem,0);transition: transform .3s ease-in-out;cursor:pointer}
-
-
-    @media screen and (max-width: 768px) {
-        .list_card {width:16.4rem}
-        .ganre_list {padding:0}
-    }
+.ganre_list .list_card {display:inline-block;margin:0 1.25rem 2.5rem 0;width:calc(100% - 80% - 1rem)}
+.ganre_list .list_card picture {display:block;border-radius:0.5rem;width:100%;max-height:31.25rem;overflow:hidden}
+.ganre_list .list_card picture img  {width:100%;height:100%}
+.ganre_list .list_card:hover {transform: translate3d(0,-0.75rem,0);transition: transform .3s ease-in-out;cursor:pointer}
+.ganre_list .list_card:nth-child(5n) {margin-right:0}
+@media screen and (max-width: 768px) {
+    .ganre_list .list_card {width:calc(100% - 68% - 0.25rem)}
+    .ganre_list .list_card:nth-child(3n) {margin-right:0}
+    .ganre_list .list_card:nth-child(5n) {margin-right:0.625rem}
+}
 </style>
